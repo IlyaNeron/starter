0c060666236768eae87b32e22c29051d13764ade
Poliakov Maksym <PoliakovMaksym@users.noreply.github.com>
2022-06-10 13:49:28 +0300

    Simple react-query example (#2)
    * Install react-query
    * Add baseUrl to typescript config
    * Implement simple react-query usage example

--- src/services/api/app/index.ts
+++ src/services/api/app/index.ts
@@ -1,8 +1,16 @@
-import apiInstance from '../api'
+import { apiInstance } from '../api'
 
 export const userApi = {
   checkConnected: async () => {
     const response = await apiInstance.get('/app/check-connected')
+
+    return response.data
+  },
+}
+
+export const postApi = {
+  getAllPosts: async () => {
+    const response = await apiInstance.get('https://jsonplaceholder.typicode.com/posts?userId=1')
 
     return response.data
   },
